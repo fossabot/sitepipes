d58331ead45148876d868b1c160f04dcf3fe9fc3
Eric Yates <eric@medleyagency.com>
2020-09-28 20:46:35 -0600

    Add project template

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('LICENSE', 'r') as f:
+    license = f.read()
+
+setup(
+    name='...',
+    version='...',
+    description='...',
+    long_description=long_description,
+    license=license,
+    author='...',
+    author_email='...',
+    url='...',
+    packages=['...'],
+)
+
