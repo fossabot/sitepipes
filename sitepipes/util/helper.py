fd99b318c7a2dd0b120ebd5f9265d296a48fd10c
Eric Yates <eric@medleyagency.com>
2020-10-31 04:18:36 -0600

    Create save / load methods

--- sitepipes/util/helper.py
+++ sitepipes/util/helper.py
@@ -1,3 +1,4 @@
+import inspect
 import uuid
 
 
@@ -15,3 +16,24 @@ def remove_id(iterable, obj_id):
 def gen_id():
     """ Generates a unique ID """
     return uuid.uuid4()
+
+
+def obj_to_module_name(obj):
+    """
+    Extracts the module name (ie. the file name) of any instance of a class
+
+    Useful for automatically detecting which libraries and frameworks are being
+    used and changing logic accordingly. Such as to detect that the model being
+    used is a Keras model and can be saved with the Keras internal API
+
+    :param obj: object - An instance of any class
+    :return: str - The module name that an object belongs to
+
+    >>> model = Model(...)
+        model_type = obj_to_module_name(model)
+    """
+
+    module = inspect.getmodule(obj)
+    base, _sep, _stem = module.__name__.partition('.')
+
+    return base
